601209ed4abc838a8492293a7e5f7c239f42bee3
renatavagapova <vagapova.rena@list.ru>
2021-11-14 20:55:38 +0300

    Выполнено 2.1

---
+++ lesson2/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson2
+{
+    class Account
+    {
+        int _number;
+
+        decimal _balance;
+
+        TypeOfBankAccount _typeAccount;
+
+        public int InfoNumber()
+        {
+            return _number;
+        }
+
+        public decimal InfoBalance()
+        {
+            return _balance;
+        }
+
+        public TypeOfBankAccount InfoTypeAccount()
+        {
+            return _typeAccount;
+        }
+
+        public void FillNumber(int number)
+        {
+            _number = number;
+        }
+
+        public void FillBalance(decimal balance)
+        {
+            _balance = balance;
+
+        }
+
+        public void FillTypeAccount(TypeOfBankAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+        }
+
+        public void FullInfo()
+        {
+            Console.WriteLine($"Тип счёта: {_typeAccount}, Номер счёта: {_number}, Баланс: {_balance}");
+        }
+    }
+}
---
+++ lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Account account = new Account();
+
+            account.FillTypeAccount(TypeOfBankAccount.DEBIT);
+
+            account.FillNumber(random.Next(1, 99999999));
+
+            account.FillBalance(100000);
+
+            account.FullInfo();
+        }
+    }
+}
