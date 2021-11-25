1d8c84b9c3be279cbff1a3b76ce6cd1096bb3571
renatavagapova <vagapova.rena@list.ru>
2021-11-25 16:44:55 +0300

    выполнила 3.2

---
+++ lesson3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesson3._2
+{
+    class Program
+    {
+        static string Rev(string str)
+        {
+            char[] reverse = str.ToCharArray();
+            Array.Reverse(reverse);
+            return new string(reverse);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            str = Rev(str);
+            Console.WriteLine(str);
+        }
+    }
+}
